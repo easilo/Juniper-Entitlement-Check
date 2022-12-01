4570c170265c5d4cf301f3f486ebb4b78a576dd7
Erin Asilo <erinasilo21@gmail.com>
2022-12-01 13:57:51 -0800

    Changed to google oath client

--- run_files/main.py
+++ run_files/main.py
@@ -29,7 +29,7 @@ import gspread
 import warnings
 import pandas as pd
 from googleapiclient.discovery import build
-from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2 import service_account
 
 
 # --------------------------------------------------------------------------
@@ -64,8 +64,9 @@ SCOPES = [
     "https://spreadsheets.google.com/feeds",
 ]
 
-creds = ServiceAccountCredentials.from_json_keyfile_name(SERVICE_ACCOUNT, scopes=SCOPES)
-gc = gspread.authorize(creds)
+creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT)
+scoped_creds = creds.with_scopes(SCOPES)
+gc = gspread.authorize(scoped_creds)
 
 spreadsheet = gc.open_by_key(SPREADSHEET_ID)
 
